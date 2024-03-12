92904ea511513ab7c002f9ef0c97b49ef1fb88bf
Javiera <javiera.quezada.leiva@gmail.com>
2024-03-12 22:47:24 +0100

    cambios realizados

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,19 +1,22 @@
-const btnSumar = document.querySelector("btnSumar");
-const btnRestar = document.querySelector("btnRestar");
+const btnSumar = document.querySelector("#btnSumar");
+const btnRestar = document.querySelector("#btnRestar");
 const resultado = document.querySelector("#resultado");
 
 btnSumar.addEventListener("click", function () {
-    // console.log("click en sumar");
-
     const numero1 = parseInt(document.querySelector("#numero1").value);
     const numero2 = parseInt(document.querySelector("#numero2").value);
-
     const sumar = numero1 + numero2;
     resultado.textContent = sumar;
-
     if (sumar < 0) {
         resultado.textContent = 0;
     }
 });
-
-
+btnRestar.addEventListener("click", function () {
+    const numero1 = parseInt(document.querySelector("#numero1").value);
+    const numero2 = parseInt(document.querySelector("#numero2").value);
+    const restar = numero1 - numero2;
+    resultado.textContent = restar;
+    if (restar < 0) {
+        resultado.textContent = 0;
+    }
+});
